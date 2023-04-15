054cb26196b6a43b3934019cdae819f46f192a99
Vivek Kumar <vivek.skumar@gmail.com>
2023-04-15 11:47:55 +0530

    Update streamlit_app.py
    Syntax error resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title('My parents new healthy Diner')
 streamlit. header( ' Breakfast Menu ' )
 streamlit. text( 'Omega 3 & Blueberry Oatmeal' )
 streamlit. text( 'Kale, Spinach & Rocket Smoothie' )
-streamlit. text( 'Hard-Boiled Free-Range Egg
+streamlit. text( 'Hard-Boiled Free-Range Egg')
