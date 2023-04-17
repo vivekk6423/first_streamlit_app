831b69d22c109678fbb56efa211a5d22b5391b74
Vivek Kumar <vivek.skumar@gmail.com>
2023-04-17 19:14:06 +0530

    added request to get data from fruity vice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My parents new healthy Diner')
 
@@ -25,4 +26,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
